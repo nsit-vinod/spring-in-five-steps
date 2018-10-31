d384eef01cc5092b581fbf4437b3c829988291dd
vinod <nsit.vinod@gmail.com>
2018-10-31 21:32:49 +0530

    basic sprin project

---
+++ spring-in-five-step/src/main/java/com/vinod/spring/basic/SpringInFiveStepApplication.java
@@ -0,0 +1,12 @@
+package com.vinod.spring.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInFiveStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInFiveStepApplication.class, args);
+	}
+}
